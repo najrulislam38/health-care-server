395ad82fc51c370b92b0fe280b9f3575e8b088bb
Najrul Islam <mdnajrulislam.4738@gmail.com>
2025-10-18 09:52:43 +0600

    added get single doctor, delete and soft delete functionality

--- src/app/modules/doctor/doctor.controller.ts
+++ src/app/modules/doctor/doctor.controller.ts
@@ -33,6 +33,39 @@ const updateIntoDB = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
+const getByIdFromDB = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await DoctorService.getByIdFromDB(id);
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: "Doctor retrieval successfully",
+    data: result,
+  });
+});
+
+const deleteFromDB = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await DoctorService.deleteFromDB(id);
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: "Doctor deleted successfully",
+    data: result,
+  });
+});
+
+const softDelete = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await DoctorService.softDelete(id);
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: "Doctor soft deleted successfully",
+    data: result,
+  });
+});
+
 const getAISuggestions = catchAsync(async (req: Request, res: Response) => {
   const result = await DoctorService.getAISuggestions(req.body);
 
@@ -46,5 +79,8 @@ const getAISuggestions = catchAsync(async (req: Request, res: Response) => {
 export const DoctorController = {
   getAll,
   updateIntoDB,
+  getByIdFromDB,
+  deleteFromDB,
+  softDelete,
   getAISuggestions,
 };
--- src/app/modules/doctor/doctor.route.ts
+++ src/app/modules/doctor/doctor.route.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import { DoctorController } from "./doctor.controller";
+import auth from "../../middlewares/auth";
+import { UserRole } from "@prisma/client";
 
 const router = express.Router();
 
@@ -7,6 +9,20 @@ router.get("/", DoctorController.getAll);
 
 router.post("/suggestion", DoctorController.getAISuggestions);
 
-router.patch("/:id", DoctorController.updateIntoDB);
+router.get("/:id", DoctorController.getByIdFromDB);
+
+router.patch(
+  "/:id",
+  auth(UserRole.ADMIN, UserRole.DOCTOR),
+  DoctorController.updateIntoDB
+);
+
+router.delete("/:id", auth(UserRole.ADMIN), DoctorController.deleteFromDB);
+
+router.delete(
+  "/soft-delete/:id",
+  auth(UserRole.ADMIN),
+  DoctorController.softDelete
+);
 
 export const doctorRouter = router;
